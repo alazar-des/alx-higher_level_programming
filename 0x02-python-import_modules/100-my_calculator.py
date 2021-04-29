bf230196b2cb9281a16f805e9242c5d881da024c
alazar-des <alazar.des@gmail.com>
2021-04-29 14:20:59 +0300

    calculate +, -, * or / of two numbers

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -12,9 +12,9 @@ if __name__ == "__main__":
             print("{} + {} = {}".format(a, b, add(a, b)))
         elif sys.argv[2] == '-':
             print("{} - {} = {}".format(a, b, sub(a, b)))
-        elif sys.argv[2] == '-':
+        elif sys.argv[2] == '*':
             print("{} * {} = {}".format(a, b, mul(a, b)))
-        elif sys.argv[2] == '-':
+        elif sys.argv[2] == '/':
             print("{} / {} = {}".format(a, b, div(a, b)))
         else:
             print("Unknown operator. Available operators: +, -, * and /")
