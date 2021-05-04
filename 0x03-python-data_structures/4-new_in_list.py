533b769a07baa1a9b0da25ec09f7b6867cf06312
alazar-des <alazar.des@gmail.com>
2021-05-04 15:12:04 +0300

    copy and change element in new list

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    """
+    Copy list and change element at index.
+    """
+    new_list = []
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+    else:
+        for i in range(len(my_list)):
+            if i != idx:
+                new_list.append(my_list[i])
+            else:
+                new_list.append(element)
+        return new_list
