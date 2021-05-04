6e344aae2111ba530856168188d021832ac53e84
alazar-des <alazar.des@gmail.com>
2021-05-04 15:41:13 +0300

    print matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    """
+    Print elements of matrix.
+    """
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            print("{:d}".format(matrix[i][j]), end=" ")
+        print()
