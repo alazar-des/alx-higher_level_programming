c3cbb6bc0e2f07900e6b4102ced07c8454215192
alazar-des <alazar.des@gmail.com>
2021-05-07 18:09:05 +0300

    delete key by value in a dictionary

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    if a_dictionary is None or (not len(a_dictionary)):
+    if a_dictionary is None:
         return
     values = list(a_dictionary.values())
     if value in values:
