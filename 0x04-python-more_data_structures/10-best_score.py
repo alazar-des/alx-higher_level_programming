871a7d778602aa876dfb458de9b47bb8341cf33b
alazar-des <alazar.des@gmail.com>
2021-05-07 16:09:15 +0300

    return best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -4,7 +4,7 @@ def best_score(a_dictionary):
     Return the best score.
     """
     if a_dictionary is None or (not len(a_dictionary)):
-        return;
+        return
     keys = list(a_dictionary.keys())
     vals = list(a_dictionary.values())
     return keys[vals.index(max(vals))]
