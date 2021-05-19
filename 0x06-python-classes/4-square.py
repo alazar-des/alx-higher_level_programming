c8d5c9015ebbf37a1a8dbe41401fca3ba02b2408
alazar-des <alazar.des@gmail.com>
2021-05-19 16:34:29 +0300

    create square class with getter and setter and calculate area

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+Define a Square class the define a square
+"""
+
+
+class Square:
+    """
+    A square class. And initialize size
+
+    Args:
+        size (int): size of square
+
+    Attributes:
+         size (int): size of square
+    """
+
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        """Get size property"""
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if type(size) is int:
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        """
+        Return the area of a square.
+        """
+        return self.__size ** 2
