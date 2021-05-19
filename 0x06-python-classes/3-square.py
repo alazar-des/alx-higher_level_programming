928dc38e9756a8eb561dbc86236e78b6037ca79b
alazar-des <alazar.des@gmail.com>
2021-05-19 16:19:34 +0300

    create square class and calculate area

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Define a Square class the define a square
+"""
+
+
+class Square:
+    """
+    A square class. And initialize size
+
+    Args:
+        size (int): size of square
+
+    Attributes:
+         size (int): size of square
+    """
+
+    def __init__(self, size=0):
+        if type(size) is int:
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        """
+        Return the area of a square.
+        """
+        return self.__size ** 2
