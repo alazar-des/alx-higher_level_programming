2e0f27e21b75fa47877dc1da83708f54c83fa5c9
alazar-des <alazar.des@gmail.com>
2021-05-19 18:25:36 +0300

    create square class with getter and setter, calculate area, and print # area times starting at position

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -17,8 +17,15 @@ class Square:
 
     def __init__(self, size=0, position=(0, 0)):
         self.__size = size
-        if type(position) is not tuple:
-            raise TypeError("position must be a tuple of 2 positive integers")
+        if type(position) is not tuple or len(position) != 2:
+            raise TypeError(
+                    "position must be a tuple of 2 positive integers")
+        if type(position[0]) is not int or type(position[1]) is not int:
+            raise TypeError(
+                    "position must be a tuple of 2 positive integers")
+        if position[0] < 0 or position[1] < 0:
+            raise TypeError(
+                    "position must be a tuple of 2 positive integers")
         self.__position = position
 
     @property
@@ -43,10 +50,6 @@ class Square:
 
     @position.setter
     def position(self, a):
-        if len(a) != 2 or type(a[0]) is not int or type(a[1]) is not \
-           int or a[0] <= 0 or a[1] <= 0:
-            raise TypeError(
-                    "position must be a tuple of 2 positive integers")
         self.__position = a
 
     def area(self):
