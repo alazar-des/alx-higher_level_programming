39e0a5647a683ab310da511856bb5888160810db
alazar-des <alazar.des@gmail.com>
2021-05-31 13:37:08 +0300

    check if object is an instance of a given class

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""checks if instance is an instance of given class"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Return True if obj is instance of a_class otherwise False.
+
+    Args:
+        obj (instance): an instance object to be checked
+
+    Yields:
+        True or False depending on the object is whether instance of the class
+        or not.
+    """
+    return isinstance(obj, a_class)
