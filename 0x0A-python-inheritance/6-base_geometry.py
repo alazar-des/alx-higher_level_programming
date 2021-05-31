6d9afbbed0fba061794debc7af9e65d1de2e9cfa
alazar-des <alazar.des@gmail.com>
2021-05-31 14:14:39 +0300

    raise an exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""A class with an area public function"""
+
+
+class BaseGeometry:
+    """A class with an area public function"""
+    def area(self):
+        """ Raise an Exception with area() is not implemented message"""
+        raise Exception("area() is not implemented")
