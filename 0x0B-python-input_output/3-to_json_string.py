1390a7b00fe0754aab156148da6d0a8d80ae9c6e
alazar-des <alazar.des@gmail.com>
2021-06-01 16:11:11 +0300

    return the json representation of an object

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import json
+"""Return the json representation of an object"""
+
+
+def to_json_string(my_obj):
+    """Return the json representation of an object.
+
+    Args:
+        my_obj (obj): python object to be returned its equivalent json
+            representation.
+
+    Yeilds:
+        json representation of python object.
+    """
+    return json.dumps(my_obj, sort_keys=True)
