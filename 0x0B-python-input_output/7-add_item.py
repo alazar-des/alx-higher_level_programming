eb326ed65b63211ca323483a21cbfd44ae701738
alazar-des <alazar.des@gmail.com>
2021-06-01 16:15:15 +0300

    save command arguments to file in list format

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import sys
+import json
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+"""Writes all command line input argument to json file."""
+
+
+if __name__ == "__main__":
+    try:
+        arg_list = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        arg_list = []
+    finally:
+        for i in range(1, len(sys.argv)):
+            arg_list.append(sys.argv[i])
+        save_to_json_file(arg_list, "add_item.json")
