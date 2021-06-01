b6c04eb241872746544990a544012a8c336c038d
alazar-des <alazar.des@gmail.com>
2021-06-01 16:13:20 +0300

    write to a file the json representation of an object

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import json
+"""Save json representation of python object in file."""
+
+
+def save_to_json_file(my_obj, filename):
+    """Save json representation of python object in file.
+    open file for write and write the json string to it. if doesn't exist
+    create it.
+
+    Args:
+        my_obj (obj): python object
+        filename (str): file name the json representation to be written to.
+    """
+    with open(filename, 'w') as file_write:
+        json.dump(my_obj, file_write)
