913ff994d7cc4943508a593b35739d230a23486b
alazar-des <77922606+alazar-des@users.noreply.github.com>
2021-06-02 15:16:08 +0300

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
+"""Write all command line arguments to json file.
+"""
+
+
 import sys
 import json
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-"""Writes all command line input argument to json file."""
-
-
 if __name__ == "__main__":
     try:
         arg_list = load_from_json_file("add_item.json")
