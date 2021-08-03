6ad7c1f02cfc36e827fb0ee96c8ce891858d0aae
alazar-des <alazar.des@gmail.com>
2021-08-03 10:15:22 +0300

    print square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -4,10 +4,10 @@ const args = process.argv.slice(2);
 if (isNaN(args[0])) {
   console.log('Missing size');
 } else {
+  for (let i = 0; i < parseInt(args[0]); i++) {
     for (let i = 0; i < parseInt(args[0]); i++) {
-	for (let i = 0; i < parseInt(args[0]); i++) {
-	    console.log('\rx');
-	}
-	console.log('');
+      process.stdout.write('X');
+    }
+    console.log('');
   }
 }
