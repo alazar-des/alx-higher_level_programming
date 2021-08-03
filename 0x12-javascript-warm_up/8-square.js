ab4b97e3d4b1b5629de71a2f59adf11fef2dfb79
alazar-des <alazar.des@gmail.com>
2021-08-03 10:06:50 +0300

    convert to integer

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const args = process.argv.slice(2);
+if (isNaN(args[0])) {
+  console.log('Missing size');
+} else {
+    for (let i = 0; i < parseInt(args[0]); i++) {
+	for (let i = 0; i < parseInt(args[0]); i++) {
+	    console.log('\rx');
+	}
+	console.log('');
+  }
+}
