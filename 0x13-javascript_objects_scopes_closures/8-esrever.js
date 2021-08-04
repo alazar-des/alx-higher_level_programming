7413cc57168eb95b58b1aa108e6699bf23f0a1e4
alazar-des <alazar.des@gmail.com>
2021-08-04 11:13:42 +0300

    reverse list

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+exports.esrever = function (list) {
+  const revList = [];
+  for (let i = list.length - 1, j = 0; i >= 0; i--, j++) {
+    revList[j] = list[i];
+  }
+  return revList;
+};
