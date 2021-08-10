4e028649fd04b30702ee23d887b78c3383de0c83
alazar-des <alazar.des@gmail.com>
2021-08-10 08:37:49 +0300

    list all rows of table

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Print records of all a table.
+"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    # Open database connection
+    db = MySQLdb.connect(host="localhost",
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3],
+                         port=3306)
+
+    # prepare a cursor object
+    cursor = db.cursor()
+
+    sql = "SELECT * FROM states ORDER BY id"
+
+    try:
+        cursor.execute(sql)
+        results = cursor.fetchall()
+        for row in results:
+            print(row)
+    except Exception:
+        print("Error: Unable to fetch.")
+
+    db.close()
