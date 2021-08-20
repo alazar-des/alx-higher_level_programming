89ae96eb3b8a6505cdfe9492d5c1fc9740fa01c2
alazar-des <77922606+alazar-des@users.noreply.github.com>
2021-08-20 17:38:51 +0300

    send email

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Python script that prints reques id
+"""
+from urllib import request, parse
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    data = parse.urlencode({'email': sys.argv[2]}).encode()
+    req = request.Request(url, data=data)
+    with request.urlopen(req) as resp:
+        html = resp.read().decode("utf-8")
+        print(html)
