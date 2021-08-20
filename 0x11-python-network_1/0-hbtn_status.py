0605b84cbab264efc99618327df8c86c9c6e626e
alazar-des <77922606+alazar-des@users.noreply.github.com>
2021-08-20 16:34:59 +0300

    print html request status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,6 +8,7 @@ if __name__ == "__main__":
     url = 'https://intranet.hbtn.io/status'
     with urlopen(url) as response:
         data = response.read()
-    print("- type: ", type(data))
-    print("- content: ", data)
-    print("- utf8 content: ", data.decode("UTF-8"))
+    print("Body response:")
+    print("\t- type: ", type(data))
+    print("\t- content: ", data)
+    print("\t- utf8 content: ", data.decode("UTF-8"))
