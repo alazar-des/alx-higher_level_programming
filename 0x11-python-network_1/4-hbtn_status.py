c10ac95de8a3644b70f568897c0e7ba3d4356ef4
alazar-des <77922606+alazar-des@users.noreply.github.com>
2021-08-20 18:27:36 +0300

    response status

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+print status of request response
+"""
+import requests
+
+
+resp = requests.get('https://intranet.hbtn.io/status')
+print("Body response:")
+print("\t- type: ", type(resp.text))
+print("\t- content: ", resp.text)
