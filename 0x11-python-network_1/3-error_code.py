dc6ce5fa22b1d0c02e54759b93a1fd99372d60bd
alazar-des <77922606+alazar-des@users.noreply.github.com>
2021-08-20 18:07:08 +0300

    handle HTTPError

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,17 +1,16 @@
-#!/usr/bin/python3                                                                                                                
-"""                                                                                                                               
-Python script that handles exception and print error code                                                                         
-"""                                                                                                                               
-from urllib import request, error                                                                                                 
-import sys                                                                                                                        
-                                                                                                                                  
-                                                                                                                                  
-if __name__ == "__main__":                                                                                                        
-    url = sys.argv[1]                                                                                                             
-    try:                                                                                                                          
-        with request.urlopen(url) as resp:                                                                                        
-            html = resp.read().decode("utf-8")                                                                                    
-            print(html)                                                                                                           
-    except error.HTTPError as e:                                                                                                  
-        print("Error code: ", e.code)                                                                                             
- 
+#!/usr/bin/python3
+"""
+Python script that handles exception and print error code
+"""
+from urllib import request, error
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    try:
+        with request.urlopen(url) as resp:
+            html = resp.read().decode("utf-8")
+        print(html)
+    except error.HTTPError as e:
+        print("Error code: ", e.code)
