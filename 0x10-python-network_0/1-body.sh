71eb4d0306e9340691cf06de5a8589b9e28304d5
alazar-des <alazar.des@gmail.com>
2021-08-24 16:55:38 +0300

    display body of a response

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 # sends a request to that URL and displays the size of the body of the response
-
-if [ "$(curl -s -o /dev/null -w "%{http_code}" "$1")" -eq 200 ]
-then
-    curl -s "$1"
-fi
+curl -s "$1"
