52e9822f70b6bb83868f2a63537b64ee6ee44e39
alazar-des <alazar.des@gmail.com>
2021-08-24 12:10:42 +0300

    send post request and print json data

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Send post request and print the json body content id and name.
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("No result")
+    else:
+        payload = {'q': sys.argv[1]}
+        resp = requests.post('http://0.0.0.0:5000/search_user', data=payload)
+
+        r_json = resp.json()
+        if r_json:
+            print("[{}] {}".format(r_json['id'], r_json['name']))
+        elif r_json == {}:
+            print("No result")
+        else:
+            print("Not a valid JSON")
