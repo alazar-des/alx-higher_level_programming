5171e3c8b5de00bf0b82036aca71fa5b55b83cce
alazar-des <alazar.des@gmail.com>
2021-08-24 15:35:06 +0300

    check number of commits fix

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -9,10 +9,9 @@ if __name__ == "__main__":
     resp = requests.get('https://api.github.com/repos/{}/{}/commits'
                         .format(sys.argv[2], sys.argv[1]))
     r_json = resp.json()
-    length = 10
-    if len(r_json) < 10:
-        lenght = len(r_json)
-    for i in range(length):
+    for i in range(len(r_json)):
+        if i >= 10:
+            break
         diction = r_json[i]
         print("{}: {}".format(diction['sha'],
                               diction['commit']['author']['name']))
