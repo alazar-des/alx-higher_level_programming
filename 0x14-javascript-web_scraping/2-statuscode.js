676c1294a2f558efb40b148ac46124b9533f3cf9
alazar-des <alazar.des@gmail.com>
2021-09-14 16:12:38 +0300

    log status code

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const request = require('request');
+const myArgs = process.argv.slice(2);
+
+request(myArgs[0], function (error, response, body) {
+  if (error) console.error('error:', error);
+  console.log('code: ', response && response.statusCode);
+});
